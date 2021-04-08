5a88a69b9d0a19ec0b8466e2e0812a4841ff25d9
Matthew Butler <matthewtbutler@gmail.com>
2021-04-07 23:19:05 -0500

    hex parsers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-import hexmap from './map'
+//import hexmap from './map'
+let _ = require('lodash')
+
+export function hexParse(hex) {
+    let hexList = hex.match(/.{1,2}/g)
+    if (hexList.length != 5) {
+        throw Error('Invalid hex code.')
+    }
+    let hexChallenge = {
+        "terrain": {
+            "type": hexList[0].charAt(0),
+            "challenge": _.toNumber(hexList[0].charAt(1))
+        },
+        "resource": {
+            "type": hexList[1].charAt(0),
+            "challenge": _.toNumber(hexList[1].charAt(1))
+        },
+        "encounter": {
+            "type": hexList[2].charAt(0),
+            "challenge": _.toNumber(hexList[2].charAt(1))
+        },
+        "location": {
+            "type": hexList[3].charAt(0),
+            "challenge": _.toNumber(hexList[3].charAt(1))
+        },
+        "treasure": {
+            "type": hexList[4].charAt(0),
+            "challenge": _.toNumber(hexList[4].charAt(1))
+        }
+    }
+        return hexChallenge    
+}
+
+export function partyBonus(party) {
+    let combat = 0, exploration = 0, diplomacy = 0, travel = 0
+    party.forEach(member => {
+        if (member == 'Warrior') {
+            combat += 2
+            exploration += 1
+        }
+
+        if (member == 'Guide') {
+            exploration += 2
+            diplomacy += 1
+        }
+
+        if (member == 'Merchant') {
+            diplomacy += 2
+            combat += 1
+        }
+
+        if (member == 'Sorcerer') {
+            combat += 1
+            diplomacy += 1
+            exploration += 1
+        }
+    })
+
+    let baseEffort = {
+        "combat": combat,
+        "exploration": exploration,
+        "diplomacy": diplomacy,
+        "travel": travel
+    }
+
+    return baseEffort
+}
 
 
--- test/sample.test.js
+++ test/sample.test.js
@@ -1,8 +1,37 @@
 import { expect } from 'chai'
-import { sample } from '../src/sample'
+import { partyBonus, hexParse } from '../src/index'
 
-describe('Sample', () => {
-    it('outputs the working confirmation message', () => {
-        expect(sample()).to.equal("It works!")
+const party1 = ['Warrior', 'Guide', 'Merchant', 'Sorcerer']
+const party2 = ['Warrior', 'Warrior']
+const party3 = ['Guide', 'Merchant', 'Librarian', 'Merchant'] // there is no librarian
+const party4 = []
+const hexCode1 = "F3G6B5X0D9"
+const hexCode2 = "F3G6B5"
+const hexCode3 = "F3G6B5X0D9E5"
+
+describe('Party Base Bonus', () => {
+    it('outputs the correct base effort for a 4-member party of each class', () => {
+        expect(partyBonus(party1)).to.eql({combat: 4, exploration: 4, diplomacy: 4, travel: 0})
+    })
+    it('outputs the correct base effort for party with duplicate classes', () => {
+        expect(partyBonus(party2)).to.eql({combat: 4, exploration: 2, diplomacy: 0, travel: 0})
+    })
+    it('outputs the correct base effort for party with an invalid class', () => {
+        expect(partyBonus(party3)).to.eql({combat: 2, exploration: 2, diplomacy: 5, travel: 0})
+    })
+    it('outputs the correct base effort for an empty party', () => {
+        expect(partyBonus(party4)).to.eql({combat: 0, exploration: 0, diplomacy: 0, travel: 0})
+    })
+})
+
+describe('Hex Parser', () => {
+    it('returns the correct hex challenge object for a given code', () => {
+        expect(hexParse(hexCode1)).to.eql({"terrain": {"type": "F", "challenge": 3},"resource": {"type": "G", "challenge": 6},"encounter": {"type": "B", "challenge": 5},"location": {"type": "X", "challenge": 0},"treasure": {"type": "D", "challenge": 9}})
+    })
+    it('throws an error if hex code is too short', () => {
+        expect(() => hexParse(hexCode2)).to.throw(Error)
+    })
+    it('throws an error if hex code is too long', () => {
+        expect(() => hexParse(hexCode3)).to.throw(Error)
     })
 })
